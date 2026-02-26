aa026e0f96e83110c2a11f05b782831ef0fa7497
Ondřej Honus <ondrejhonus@proton.me>
2026-02-26 15:12:01 +0100

    allow selecting header for sorting, gonna do sorting

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 For now, this is just a program, that lists all the running processes on a Linux system.
 Later, it will be more similar to a task manager like htop.
 
+## Functions
+- See all running processes
+- See the processes' PID, name, state, memory usage
+- See the process states (Sleeping, Running, Zombie)
+- Send a SIGnal to kill or controll a specific process
+
+## TO-DO\
+- CPU usage
+- Order processes by other factors than PID
+- Search by several factors
+- Filters
+
+
 ## Compilation and Execution
 To compile and run the program, use the provided `compile-run.sh` script:
 ```bash
--- src/ui.cpp
+++ src/ui.cpp
@@ -37,23 +37,23 @@ cpm proc_man;
 */
 
 Component ModalComponent(
-                        std::function<void()> sighup,
-                        std::function<void()> sigint,
-                        std::function<void()> sigquit,
-                        std::function<void()> sigill,
-                        std::function<void()> sigtrap,
-                        std::function<void()> sigabrt,
-                        std::function<void()> sigbus,
-                        std::function<void()> sigfpe,
-                        std::function<void()> sigkill,
-                        std::function<void()> sigusr1,
-                        std::function<void()> sigegv,
-                        std::function<void()> sigusr2,
-                        std::function<void()> sigpipe,
-                        std::function<void()> sigalrm,
-                        std::function<void()> sigterm,
-                        std::function<void()> hide_modal
-                        ) {
+    std::function<void()> sighup,
+    std::function<void()> sigint,
+    std::function<void()> sigquit,
+    std::function<void()> sigill,
+    std::function<void()> sigtrap,
+    std::function<void()> sigabrt,
+    std::function<void()> sigbus,
+    std::function<void()> sigfpe,
+    std::function<void()> sigkill,
+    std::function<void()> sigusr1,
+    std::function<void()> sigegv,
+    std::function<void()> sigusr2,
+    std::function<void()> sigpipe,
+    std::function<void()> sigalrm,
+    std::function<void()> sigterm,
+    std::function<void()> hide_modal)
+{
     auto component = Container::Vertical({
         Button("[0] Cancel", hide_modal, button_style),
         Button("[1] SIGHUP", sighup, button_style),
@@ -74,14 +74,12 @@ Component ModalComponent(
 
     });
     component |= Renderer([&](Element inner)
-                          {
-                              return vbox({
-                                         text("Select signal to send:") | center | bold,
-                                         separator(),
-                                         inner,
-                                     })                              //
-                                     | size(WIDTH, GREATER_THAN, 30) | frame | vscroll_indicator | size(HEIGHT, LESS_THAN, 30) | border; 
-                          });
+                          { return vbox({
+                                       text("Select signal to send:") | center | bold,
+                                       separator(),
+                                       inner,
+                                   }) |
+                                   size(WIDTH, GREATER_THAN, 30) | frame | vscroll_indicator | size(HEIGHT, LESS_THAN, 30) | border; });
     return component;
 }
 
@@ -91,16 +89,19 @@ int pidof_selected(int selected_row)
     return procs[selected_row].pid;
 }
 
-void handle_proc_kill(int selected_row) {
-    
+void handle_proc_kill(int selected_row)
+{
 }
 
 int ui::draw_ui()
 {
-    auto screen = ScreenInteractive::Fullscreen();
+    auto screen = ScreenInteractive::TerminalOutput();
 
     int selected_row = 0;
     int total_rows = 0;
+
+    int selected_col = 2;
+    const int total_cols = 4;
 
     auto table_content = Renderer([&]()
                                   {
@@ -109,6 +110,14 @@ int ui::draw_ui()
 
         std::vector<std::vector<std::string>> rows;
         for (int i = 0; i < total_rows; ++i) {
+            if (i == 0) {
+                rows.push_back({
+                    "[PID]",
+                    "[NAME]",
+                    "[STATE]",
+                    "[MEMORY]"
+                });
+            }
             const auto& p = procs[i];
             rows.push_back({
                 std::to_string(p.pid), 
@@ -127,14 +136,19 @@ int ui::draw_ui()
         table.SelectRow(0).Decorate(bold);
         table.SelectRow(0).SeparatorVertical(LIGHT);
 
+        table.SelectColumns(0, 3).Decorate(flex_grow);
+
         if (total_rows > 0) {
-            selected_row = std::max(0, std::min(selected_row, total_rows - 1));
-
+            // Make sure selected_row is at least 1 to skip header row
+            selected_row = std::max(0, std::min(selected_row, total_rows + 1));
+            if (selected_row != 0){
             table.SelectRow(selected_row).DecorateCells(inverted);
-
             table.SelectRow(selected_row).Decorate(focus);
-            table.SelectRow(selected_row).Decorate(focusCursorBlock);
+            }
+            table.SelectCell(selected_col, 0).Decorate(inverted);
+            table.SelectCell(selected_col,0).Decorate(focus);
         }
+
 
         return table.Render() | vscroll_indicator | yframe | flex; });
 
@@ -158,28 +172,44 @@ int ui::draw_ui()
                                           separator(),
                                           table_content->Render() | flex,
                                           separator(),
-                                          text("[q]uit | [k]ill process") | center}) |
+                                          text("[q]uit | [Shift]+[k]ill process") | center}) |
                                     border; });
 
+    // define kill proc. menu
     auto show_modal = [&]
     { modal_shown = true; };
     auto hide_modal = [&]
     { modal_shown = false; };
-    auto sighup = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGHUP);};
-    auto sigint = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGINT);};
-    auto sigquit = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGQUIT);};
-    auto sigill = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGILL);};
-    auto sigtrap = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGTRAP);};
-    auto sigabrt = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGABRT);};
-    auto sigbus = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGBUS);};
-    auto sigfpe = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGFPE);};
-    auto sigkill = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGKILL);};
-    auto sigusr1 = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGUSR1);};
-    auto sigev = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGSEGV);};
-    auto sigusr2 = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGUSR2);};
-    auto sigpipe = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGPIPE);};
-    auto sigalrm = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGALRM);};
-    auto sigterm = [&] {proc_man.kill_proc(pidof_selected(selected_row), SIGTERM);};
+    auto sighup = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGHUP); };
+    auto sigint = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGINT); };
+    auto sigquit = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGQUIT); };
+    auto sigill = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGILL); };
+    auto sigtrap = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGTRAP); };
+    auto sigabrt = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGABRT); };
+    auto sigbus = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGBUS); };
+    auto sigfpe = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGFPE); };
+    auto sigkill = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGKILL); };
+    auto sigusr1 = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGUSR1); };
+    auto sigev = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGSEGV); };
+    auto sigusr2 = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGUSR2); };
+    auto sigpipe = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGPIPE); };
+    auto sigalrm = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGALRM); };
+    auto sigterm = [&]
+    { proc_man.kill_proc(pidof_selected(selected_row), SIGTERM); };
 
     auto exit = screen.ExitLoopClosure();
     auto do_nothing = [&] {};
@@ -201,19 +231,31 @@ int ui::draw_ui()
             screen.Exit();
             return true;
         }
-        if (event == Event::ArrowUp || (event.is_mouse() && event.mouse().button == Mouse::WheelUp))
+
+        if (event == Event::ArrowUp || event == Event::k || (event.is_mouse() && event.mouse().button == Mouse::WheelUp))
         {
             if (selected_row > 0 && !modal_shown)
             {
                 selected_row--;
                 return true;
             }
-            else
-            {
-                return false;
-            }
+            return false;
         }
-        if (event == Event::ArrowDown || (event.is_mouse() && event.mouse().button == Mouse::WheelDown)) {
+        if (event == Event::l || event == Event::ArrowRight) {
+            if (selected_col <= total_cols - 1) {
+                selected_col++;
+                return true;
+            }
+            return false;
+        }
+         if (event == Event::j || event == Event::ArrowLeft) {
+            if (selected_col >= 0) {
+                selected_col--;
+                return true;
+            }
+            return false;
+        }
+        if (event == Event::ArrowDown || event == Event::j || (event.is_mouse() && event.mouse().button == Mouse::WheelDown)) {
         if (selected_row < total_rows - 1 && !modal_shown)
         {
             selected_row++;
@@ -223,9 +265,18 @@ int ui::draw_ui()
         return false;
         }
 
-        if (event == Event::Character('k')) {
+        if (event == Event::K) {
         modal_shown == true ? hide_modal() : show_modal();
         return true;
+        }
+        if (event == Event::PageDown)
+        {
+            selected_row = total_rows;
+            return true;
+        }
+        if (event == Event::PageUp) {
+            selected_row = 1;
+            return true;
         }
 
         return false; });
